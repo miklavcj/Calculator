6ce2662a70f4ee485f8f1116b3a6c774292c1ba1
miklavcj <jaka.miklavc@gmail.com>
2020-10-14 20:03:49 +0200

    Added new UI

--- script.js
+++ script.js
@@ -1,102 +1,95 @@
 const display = document.querySelector("#main-display");
-let buttons = document.querySelectorAll("button");
+let buttons = document.querySelectorAll(".btn");
 let num1 = null;
 let num2 = null;
 let op = "";
 let lastKey = null;
 
+buttons.forEach((btn) => btn.addEventListener("click", updateDisplay));
 
-buttons.forEach(btn => btn.addEventListener('click', updateDisplay));
+function updateDisplay(key) {
+  let keyValue = key.target.textContent;
 
+  if (!isNaN(keyValue)) {
+    if (lastKey === null || lastKey === "." || !isNaN(lastKey)) {
+      display.append(keyValue);
+    } else if (isNaN(lastKey)) {
+      display.textContent = "";
+      display.append(keyValue);
+    }
+  }
 
-function updateDisplay (key) {
-	let keyValue = key.target.textContent;
+  if (isNaN(keyValue)) {
+    let checkDot = display.textContent;
 
-	if(!isNaN(keyValue)) {
-		if (lastKey === null || lastKey === '.' || !isNaN(lastKey)) {
-			display.append(keyValue);
-			} else if (isNaN(lastKey)) {
-				display.textContent = "";
-				display.append(keyValue);
-			}
-		
-	}
+    if (
+      keyValue === "+" ||
+      keyValue === "-" ||
+      keyValue === "x" ||
+      keyValue === "÷"
+    ) {
+      num1 = Number(display.textContent);
+      op = keyValue;
+      display.textContent = op;
+    } else if (keyValue === ".") {
+      if (display.textContent.includes(".")) {
+        return;
+      } else {
+        display.append(keyValue);
+      }
+    } else if (keyValue === "=") {
+      num2 = Number(display.textContent);
+      let result = operate(op, num1, num2);
+      result = +result.toFixed(2);
+      display.textContent = result;
+      num1 = result;
 
-	if(isNaN(keyValue)) {
-	  
-	  let checkDot = display.textContent;
-	  
-		if(keyValue === "+" ||
-		  keyValue === "-" ||
-			keyValue === "x" ||
-			keyValue === "÷") {
-				num1 = Number(display.textContent);
-				op = keyValue;
-				display.textContent = op;
-
-				
-			} else if (keyValue === ".") {
-			    if (display.textContent.includes('.')) { 
-			    return ;
-			   } else {
-				  display.append(keyValue); }
-			
-				
-		    } else if (keyValue === '=') {
-				num2 = Number(display.textContent);
-				let result = operate(op,num1,num2);
-				display.textContent = result;
-				num1 = result
-
-			  console.log(num1);
-				console.log(num2);
-				console.log(op);
-			} else if (keyValue === 'C') {
-				clear();
-			}
-	} 
-	lastKey = keyValue;
-};
-
-
-function clear() {
-	display.textContent ="";
-	num1 = num2 = null;
-	op = "";
-	lastKey = null;
+      console.log(num1);
+      console.log(num2);
+      console.log(op);
+    } else if (keyValue === "C") {
+      clear();
+    }
+  }
+  lastKey = keyValue;
 }
 
-let add = function(a,b) {
-	return a + b;
+function clear() {
+  display.textContent = "";
+  num1 = num2 = null;
+  op = "";
+  lastKey = null;
+}
+
+let add = function (a, b) {
+  return a + b;
 };
 
-let substract = function(a,b) {
-	return a-b;
+let substract = function (a, b) {
+  return a - b;
 };
 
-let multiply = function(a,b) {
-	return a*b;
+let multiply = function (a, b) {
+  return a * b;
 };
 
-let divide = function(a,b) {
-	return a/b;
+let divide = function (a, b) {
+  return a / b;
 };
 
-
-
-let operate = function(op,a,b) {
-	switch(op) {
-		case '+':
-			return add(a,b);
-			break;
-		case '-':
-			return substract(a,b);
-			break;
-		case 'x':
-			return multiply(a,b);
-			break;
-		case '÷':
-			return divide(a,b);
-			break;
-	};
+let operate = function (op, a, b) {
+  switch (op) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return substract(a, b);
+      break;
+    case "x":
+      return multiply(a, b);
+      break;
+    case "÷":
+      return divide(a, b);
+      break;
+  }
 };
